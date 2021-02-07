dc50192dc1087f48828b176180ede8831f9c679f
Flyduckbb <gogo5208@gmail.com>
2021-02-07 20:55:27 +0900

    1010번 -> 정수1개 입력받아 그대로 출력

---
+++ CodeUp/src/codeUp/Main_1010.java
@@ -0,0 +1,17 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1010 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a;
+		a = sc.nextInt();
+		
+		System.out.print(a);
+
+	}
+
+}
