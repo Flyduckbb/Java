252a9f7f9293e85399b581459d6bfa356bcc2b29
Flyduckbb <gogo5208@gmail.com>
2021-02-08 22:32:03 +0900

    1014번 -> 문자2개 입력받아 순서 바꿔 출력

---
+++ CodeUp/src/codeUp/Main_1014.java
@@ -0,0 +1,16 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1014 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String ch1 = sc.next();
+		String ch2 = sc.next();
+		
+		System.out.printf("%s %s", ch2, ch1);
+	}
+
+}
