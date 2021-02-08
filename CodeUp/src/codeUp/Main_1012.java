70cd6bfca53dac2e343143c9bd7d93dcc09d2f1d
Flyduckbb <gogo5208@gmail.com>
2021-02-08 18:44:51 +0900

    1012번 -> 실수1개 입력받아 그대로 출력

---
+++ CodeUp/src/codeUp/Main_1012.java
@@ -0,0 +1,15 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1012 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double num = sc.nextDouble();
+		
+		System.out.printf("%.6f", num);
+	}
+
+}
