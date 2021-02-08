294fe1408890c57f51c2962550b0699678a34d15
Flyduckbb <gogo5208@gmail.com>
2021-02-08 18:39:59 +0900

    1011번 -> 문자1개 입력받아 그대로 출력

---
+++ CodeUp/src/codeUp/Main_1011.java
@@ -0,0 +1,15 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1011 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String ch = sc.next();
+		
+		System.out.print(ch);
+	}
+
+}
