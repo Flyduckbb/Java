4c51241889bc80497085768eb1382c793fb54aee
Flyduckbb <gogo5208@gmail.com>
2021-02-08 23:18:41 +0900

    1015번 -> 실수 입력받아 둘째자리까지 출력

---
+++ CodeUp/src/codeUp/Main_1015.java
@@ -0,0 +1,15 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1015 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		float num1 = sc.nextFloat();
+		
+		System.out.printf("%.2f", num1);
+	}
+
+}
