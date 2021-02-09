4ee393e3582e004a3f4c2e614dbf6b976306d2e3
Flyduckbb <gogo5208@gmail.com>
2021-02-09 21:30:21 +0900

    1021번 -> 단어1개 입력받아 그대로 출력

---
+++ CodeUp/src/codeUp/Main_1021.java
@@ -0,0 +1,17 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1021 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String word = sc.next();
+		
+		System.out.print(word);
+		
+		sc.close();
+	}
+
+}
