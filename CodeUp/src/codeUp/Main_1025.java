9990daef1422b995f7cef6488a744f3a7a84b1da
Flyduckbb <gogo5208@gmail.com>
2021-02-11 22:25:50 +0900

    1025번 -> 정수1개 입력받아 나누어 출력

---
+++ CodeUp/src/codeUp/Main_1025.java
@@ -0,0 +1,22 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1025 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String num = sc.next();
+		String[] num_s = num.split("");
+		
+		for(int i=0; i<num_s.length; i++)
+		{
+			int num_i = Integer.valueOf(num_s[i]);
+			double power = Math.pow(10.0, i);
+			num_i = num_i * (10000/(int)power);
+			System.out.println("[" + num_i+ "]");
+		}
+		
+	}
+}
