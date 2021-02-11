e4e3f52a0f46a7e32a844dbedb36af6a0d4816dd
Flyduckbb <gogo5208@gmail.com>
2021-02-11 23:25:58 +0900

    1030번 -> 정수1개 입력받아 그대로 출력3

---
+++ CodeUp/src/codeUp/Main_1030.java
@@ -0,0 +1,17 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1030 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long num = sc.nextLong();
+		
+		System.out.print(num);
+		
+		sc.close();
+	}
+
+}
