1e1d75fd312103e21bc2f4349ed6e71184a92f15
Flyduckbb <gogo5208@gmail.com>
2021-02-13 23:46:54 +0900

    1036번 -> 영문자 1개 입력 10진수 출력

---
+++ CodeUp/src/codeUp/Main_1036.java
@@ -0,0 +1,19 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1036 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char alph = sc.nextLine().charAt(0);
+		
+		int ialph = (int)alph;
+		
+		System.out.print(ialph);
+		
+		sc.close();
+	}
+
+}
