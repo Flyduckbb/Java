d774e274d1d7fe48c3ca862679092b0eb325050b
Flyduckbb <gogo5208@gmail.com>
2021-02-13 22:52:48 +0900

    1032번 -> 10진정수 입력 16진수로 출력

---
+++ CodeUp/src/codeUp/Main_1032.java
@@ -0,0 +1,17 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1032 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		System.out.printf("%x", num);
+		
+		sc.close();
+	}
+
+}
