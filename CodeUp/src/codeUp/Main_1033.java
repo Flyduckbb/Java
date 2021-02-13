7020b62057e66de42269dc72185de99aad944d0d
Flyduckbb <gogo5208@gmail.com>
2021-02-13 22:54:19 +0900

    1033번 -> 10진정수 입력 16진수 출력2

---
+++ CodeUp/src/codeUp/Main_1033.java
@@ -0,0 +1,17 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1033 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		System.out.printf("%X", num);
+		
+		sc.close();
+	}
+
+}
