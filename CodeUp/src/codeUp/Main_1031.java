1ac94d634dc951ffc72587b937984da963ee07db
Flyduckbb <gogo5208@gmail.com>
2021-02-13 22:49:28 +0900

    1031번 -> 10진정수 1개입력 8진수로 출력

---
+++ CodeUp/src/codeUp/Main_1031.java
@@ -0,0 +1,17 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1031 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		System.out.printf("%o", num);
+		
+		sc.close();
+	}
+
+}
