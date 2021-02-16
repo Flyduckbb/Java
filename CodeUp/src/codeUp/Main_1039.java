90a7a832146d8b963a02fcc4467cecc844af7479
Flyduckbb <gogo5208@gmail.com>
2021-02-16 10:50:05 +0900

    1039번 -> 정수2개 입력받아 합 출력2

---
+++ CodeUp/src/codeUp/Main_1039.java
@@ -0,0 +1,16 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1039 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long num1 = sc.nextLong();
+		long num2 = sc.nextLong();
+		
+		System.out.print(num1 + num2);
+		sc.close();
+	}
+}
