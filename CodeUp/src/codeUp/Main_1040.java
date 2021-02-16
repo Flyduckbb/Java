0c9c62b1b6b38f2ff807ac09424325dced361e2a
Flyduckbb <gogo5208@gmail.com>
2021-02-16 10:52:44 +0900

    1040번 -> 정수1개 입력받아 부호바꿔 출력

---
+++ CodeUp/src/codeUp/Main_1040.java
@@ -0,0 +1,16 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1040 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long num1 = sc.nextLong();
+		
+		System.out.print(-num1);
+		sc.close();
+	}
+
+}
