5620cc20db5cd2ac185aac95a73166b0d882c5bb
Flyduckbb <gogo5208@gmail.com>
2021-02-16 10:35:04 +0900

    1037번 -> 정수 입력받아 아스키문자로 출력

---
+++ CodeUp/src/codeUp/Main_1037.java
@@ -0,0 +1,17 @@
+ package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1037 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		char ch = (char)num;
+		
+		System.out.print(ch);
+		sc.close();
+	}
+
+}
