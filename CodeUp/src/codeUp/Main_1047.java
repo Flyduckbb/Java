60391c503757cd41bb9f2aea6691fb468e0b3134
Flyduckbb <gogo5208@gmail.com>
2021-02-17 19:08:38 +0900

    1047번 -> 정수1개 입력받아 2배곱해 출력

---
+++ CodeUp/src/codeUp/Main_1047.java
@@ -0,0 +1,17 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1047 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long num1 = sc.nextInt();
+		
+		System.out.print(num1<<1);
+		
+		sc.close();
+	}
+
+}
