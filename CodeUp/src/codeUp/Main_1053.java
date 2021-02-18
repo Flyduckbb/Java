cd4bbc5ce8690bfa86866ffb4247a8aaebfd2bf6
Flyduckbb <gogo5208@gmail.com>
2021-02-18 16:06:31 +0900

    1053번 -> 참 거짓 바꾸기

---
+++ CodeUp/src/codeUp/Main_1053.java
@@ -0,0 +1,15 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1053 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		System.out.print(num==1?0:1);
+		sc.close();
+	}
+
+}
