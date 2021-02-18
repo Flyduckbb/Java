4f0428296c7477f7df6229de455555d99ec4dfd3
Flyduckbb <gogo5208@gmail.com>
2021-02-18 18:28:01 +0900

    1061번 -> 비트단위로 OR하여 출력

---
+++ CodeUp/src/codeUp/Main_1061.java
@@ -0,0 +1,17 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1061 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long num1 = sc.nextInt();
+		long num2 = sc.nextInt();
+		
+		System.out.printf("%d",  num1|num2);
+		sc.close();
+	}
+
+}
