55d90565d8e251a3a6a4fa6d9ad0af6ee1f9238f
Flyduckbb <gogo5208@gmail.com>
2021-02-19 16:19:23 +0900

    1071번 -> 0 입력될 때까지 무한 출력

---
+++ CodeUp/src/codeUp/Main_1071.java
@@ -0,0 +1,20 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1071 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true)
+		{
+			long num = sc.nextInt();
+			
+			if(num==0)
+				break;
+			System.out.println(num);
+		}
+	}
+
+}
