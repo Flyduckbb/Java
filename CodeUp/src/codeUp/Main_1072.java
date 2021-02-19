5d4d9b44152179ff6e358e829c4ebe0505cc5229
Flyduckbb <gogo5208@gmail.com>
2021-02-19 16:23:05 +0900

    1072번 -> 정수 입력받아 계속 출력

---
+++ CodeUp/src/codeUp/Main_1072.java
@@ -0,0 +1,21 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1072 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int cnt = sc.nextInt();
+		
+		for(int i=0; i<cnt; i++)
+		{
+			long num = sc.nextInt();
+			System.out.println(num);
+		}
+		
+		sc.close();
+	}
+
+}
