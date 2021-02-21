73df847abc2d9a2e135a33eb8a13b98f401eb196
Flyduckbb <gogo5208@gmail.com>
2021-02-22 00:12:54 +0900

    1076번 -> 문자1개 입력받아 알파벳 출력

---
+++ CodeUp/src/codeUp/Main_1076.java
@@ -0,0 +1,17 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1076 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char word = sc.nextLine().charAt(0);
+		
+		for(char i = 'a'; i<=word; i++)
+			System.out.print(i+" ");
+		
+		sc.close();
+	}
+}
