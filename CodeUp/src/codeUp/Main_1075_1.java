6dec9931c7361dc90649b59df18df75faf69f1b1
Flyduckbb <gogo5208@gmail.com>
2021-02-21 23:58:41 +0900

    1075번 -> 다른버전

---
+++ CodeUp/src/codeUp/Main_1075_1.java
@@ -0,0 +1,21 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1075_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int cnt = sc.nextInt();
+		
+		while(cnt!=0)
+		{
+			cnt--;
+			System.out.println(cnt);
+		}
+		
+		sc.close();
+	}
+
+}
