be2240cf62fd59a264ac28553b1c390c8c8db086
Flyduckbb <gogo5208@gmail.com>
2021-02-22 21:18:17 +0900

    1077번 -> 정수1개 입력받아 그 수까지 출력

---
+++ CodeUp/src/codeUp/Main_1077.java
@@ -0,0 +1,18 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1077 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int cnt = sc.nextInt();
+		
+		for(int i=0; i<=cnt; i++)
+			System.out.println(i);
+		
+		sc.close();
+	}
+
+}
