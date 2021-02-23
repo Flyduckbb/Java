7d66119fadfd56172cef78ca8a343ca251725b00
Flyduckbb <gogo5208@gmail.com>
2021-02-23 19:08:49 +0900

    1085번 -> 소리 파일 저장용량 계산

--- CodeUp/src/codeUp/Main_1085.java
+++ CodeUp/src/codeUp/Main_1085.java
@@ -1,9 +1,22 @@
 package codeUp;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main_1085 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		long h = sc.nextInt();
+		long b = sc.nextInt();
+		long c = sc.nextInt();
+		long s = sc.nextInt();
+		
+		double sum = h*b*c*s;
+		
+		double result = (sum/8)/Math.pow(2, 10)/Math.pow(2, 10);
+		System.out.format("%.1f MB", result);
+		sc.close();
 	}
 
 }
