313e1e41b18d9b76328a682424dcd0e938791cbb
Flyduckbb <gogo5208@gmail.com>
2021-02-24 18:30:31 +0900

    1087번 -> 여기까지! 이제 그만~

---
+++ CodeUp/src/codeUp/Main_1087.java
@@ -0,0 +1,24 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class Main_1087 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int sum = 0;
+		
+		for(int i=1; i<=num; i++)
+		{
+			sum+=i;
+			if(sum>=num)
+				break;
+		}
+		
+		System.out.print(sum);
+		sc.close();
+	}
+
+}
